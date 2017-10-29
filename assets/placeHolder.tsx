5c120bee4b20593b94d7b1a2d5f0c77bf25c10cb
Ethan <epestellmoyer@gmail.com>
2017-10-29 16:01:41 -0400

    Added placeholder tilemap

---
+++ assets/placeHolder.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="placeHolder" tilewidth="32" tileheight="32" tilecount="2" columns="2">
+ <image source="Tiles.png" width="64" height="32"/>
+</tileset>
--- src/States/Debug.ts
+++ src/States/Debug.ts
@@ -1,6 +1,6 @@
 export class Debug extends Phaser.State {
     debug: Phaser.Sprite
-
+    cursors: Phaser.CursorKeys;
     preload() {
         // this.load.image("preloadBar", "./assets/startbar.png");
         // this.load.image("preloadBarBg", "./assets/prloadBar.png");
@@ -9,5 +9,13 @@ export class Debug extends Phaser.State {
     create() {
         this.debug = this.add.sprite(this.game.world.centerX, this.game.world.centerY, "debug");
         this.debug.anchor.set(0.5, 0.5);
+
+        this.cursors = this.game.input.keyboard.createCursorKeys();
+    }
+
+    update() {
+        if (this.cursors.up.isDown) {
+            this.debug.visible = false;
+        }
     }
 }
