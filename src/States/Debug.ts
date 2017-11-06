65accf9547c49aab9bb0e3f25ed01f7af28245a8
Ethan <epestellmoyer@gmail.com>
2017-11-05 20:04:02 -0500

    Finished player sprite.

--- src/States/Debug.ts
+++ src/States/Debug.ts
@@ -3,6 +3,10 @@ export class Debug extends Phaser.State {
     private player: Phaser.Sprite;
     private walls: Phaser.TilemapLayer;
     private speed: number;
+    private upAnim: Phaser.Animation;
+    private downAnim: Phaser.Animation;
+    private leftAnim: Phaser.Animation;
+    private rightAnim: Phaser.Animation;
     // pSpawn: Phaser.Group;
 
     public preload() {
@@ -21,7 +25,12 @@ export class Debug extends Phaser.State {
         map.setCollision(1, true, "Walls");
         this.game.physics.enable(this.walls);
 
-        this.player = this.game.add.sprite(32, 96, "debug-player");
+        this.player = this.game.add.sprite(32, 96, "playerSpritesheet");
+        this.upAnim = this.player.animations.add('upAnim', [3,4,5], 5, true);
+        this.downAnim = this.player.animations.add('downAnim', [0,1,2], 5, true);
+        this.leftAnim = this.player.animations.add('leftAnim', [9,10,11], 5, true);
+        this.rightAnim = this.player.animations.add('rightAnim', [6,7,8], 5, true);
+
         this.game.physics.enable(this.player);
         this.player.body.collideWorldBounds = true;
 
@@ -37,15 +46,19 @@ export class Debug extends Phaser.State {
         velocity.set(0);
         if(this.cursors.up.isDown) {
             velocity.y -= this.speed;
+            this.player.play("upAnim");
         }
         else if(this.cursors.down.isDown) {
             velocity.y += this.speed;
+            this.player.play("downAnim");
         }
         if(this.cursors.left.isDown) {
             velocity.x -= this.speed;
+            this.player.play("leftAnim");
         }
         else if(this.cursors.right.isDown) {
             velocity.x += this.speed;
+            this.player.play("rightAnim");
         }
     }
 }
--- src/States/Preloader.ts
+++ src/States/Preloader.ts
@@ -10,6 +10,7 @@ export class Preloader extends Phaser.State {
         this.load.tilemap("debug", "assets/debug-map.json", null, Phaser.Tilemap.TILED_JSON);
         this.load.image("debug-player", "assets/debug-player.png");
         this.load.image("debug-tileset", "assets/Tiles.png");
+        this.load.spritesheet("playerSpritesheet", "assets/player.png", 20, 32, 12);
     }
 
     public create() {
